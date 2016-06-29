9254f96125e28e28fb57e2df133edd268a1dc31e
thilinaviraj <thilinaviraj950@gmail.com>
2016-06-29 19:20:54 +0530

    CRUD operations for Room

---
+++ assetmanager/roomdetails.php
@@ -0,0 +1,391 @@
+<?php
+require_once 'conection.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>AMS</title>
+
+  <!-- Bootstrap core CSS -->
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="fonts/css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet">
+
+  <!-- Custom styling plus plugins -->
+  <link href="css/custom.css" rel="stylesheet">
+  <link href="css/icheck/flat/green.css" rel="stylesheet">
+
+  <link href="js/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+  <link href="js/datatables/buttons.bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="js/datatables/fixedHeader.bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="js/datatables/responsive.bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="js/datatables/scroller.bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+  <script src="js/jquery.min.js"></script>
+
+  <!--Search and sort!-->
+  <script src="sorttable.js"></script>
+  <script>
+ $(document).ready(function() {
+    var activeSystemClass = $('.list-group-item.active');
+
+    //something is entered in search form
+    $('#system-search').keyup( function() {
+       var that = this;
+        // affect all table rows on in systems table
+        var tableBody = $('.table-list-search tbody');
+        var tableRowsClass = $('.table-list-search tbody tr');
+        $('.search-sf').remove();
+        tableRowsClass.each( function(i, val) {
+        
+            //Lower text for case insensitive
+            var rowText = $(val).text().toLowerCase();
+            var inputText = $(that).val().toLowerCase();
+            if(inputText != '')
+            {
+                $('.search-query-sf').remove();
+                tableBody.prepend('<tr class="search-query-sf"><td colspan="6"><strong>Searching for: "'
+                    + $(that).val()
+                    + '"</strong></td></tr>');
+            }
+            else
+            {
+                $('.search-query-sf').remove();
+            }
+
+            if( rowText.indexOf( inputText ) == -1 )
+            {
+                //hide rows
+                tableRowsClass.eq(i).hide();
+                
+            }
+            else
+            {
+                $('.search-sf').remove();
+                tableRowsClass.eq(i).show();
+            }
+        });
+        //all tr elements are hidden
+        if(tableRowsClass.children(':visible').length == 0)
+        {
+            tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
+        }
+    });
+});
+  </script>
+</head>
+
+
+<body class="nav-md">
+<script src="js/jquery.min.js"></script>
+
+
+  <div class="container body">
+
+
+    <div class="main_container">
+
+      <div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+
+          
+          <div class="clearfix"></div>
+
+          <!-- menu prile quick info -->
+          <div class="profile">
+            <div class="profile_pic">
+              <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+            </div>
+            <div class="profile_info">
+              <span>Welcome,</span>
+              <h2>chathura</h2>
+            </div>
+          </div>
+          <!-- /menu prile quick info -->
+
+          <br />
+
+          <!-- sidebar menu -->
+          <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+
+            <div class="menu_section">
+           <!--   <h3>General</h3> -->
+              <ul class="nav side-menu">
+			  
+               
+                
+				<li><a href="diviassetclerck.html"><i class="fa fa-home"></i> Home </span></a></li>
+				<li><a href="createdivision.html"><i class="fa fa-building"></i> Item Category Details </span></a></li>
+				
+				
+				
+               </ul>
+            </div>
+
+          </div>
+          <!-- /sidebar menu -->
+
+         
+        </div>
+      </div>
+
+      <!-- top navigation -->
+      <div class="top_nav">
+
+        <div class="nav_menu">
+          <nav class="" role="navigation">
+            <div class="nav toggle">
+              <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+			<li class="">
+                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                 chathura
+                  <span class=" fa fa-angle-down"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-usermenu pull-right">
+                  <li><a href="javascript:;">  Profile</a>
+                  </li>
+                <!--  <li>
+                    <a href="javascript:;">Help</a>
+                  </li> -->
+                  <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                  </li>
+                </ul>
+              </li>
+              
+
+              <li role="presentation" class="dropdown">
+                <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                  <i class="fa fa-envelope-o"></i>
+                  <span class="badge bg-green">6</span>
+                </a>
+                <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                  <li>
+                    <a>
+                      <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>chathura</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                        add a asset to the system
+                                    </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span class="image">
+                                        <img src="images/img.jpg" alt="Profile Image" />
+                                    </span>
+                      <span>
+                                        <span>chathura</span>
+                      <span class="time">3 mins ago</span>
+                      </span>
+                      <span class="message">
+                                       add a asset to the system
+                                    </span>
+                    </a>
+                  </li>
+                 
+                  
+                  <li>
+                    <div class="text-center">
+                      <a>
+                        <strong>See All Alerts</strong>
+                        <i class="fa fa-angle-right"></i>
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+
+            </ul>
+          </nav>
+        </div>
+
+      </div>
+      <!-- /top navigation -->
+
+      <!-- page content -->
+      <div class="right_col" role="main">
+        <div class="">
+          <div class="page-title">
+
+			
+          </div>
+          <div class="clearfix"></div>
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h2> Room Details</h2>
+                  
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                </div>
+                  <form name="editItemcategory" method="POST" action="edit_itemcategory.php">
+                <input class="" id="system-search" name="q" placeholder="Search for" required>
+                    <br> <br>  
+                <table id="datatable" class="table table-striped table-list-search table-bordered sortable">
+                    <thead>
+                        <tr>
+                            <th>Room Code</th>
+                            <th>Room Name</th>
+                            <th>Division</th>
+                            <th>Description</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        
+                        </tr>
+                    </thead>
+
+
+                    <tbody>
+                        <?php
+                        mysqli_select_db($conn, "asm");
+                        $res= "SELECT Room_code,Room_name,Division,Description FROM room";
+                        $result= $conn->query($res);
+                        ?>
+                        <tr class="success">
+                            <?php
+                            if($result->num_rows > 0){
+                                while($row= $result->fetch_assoc()){
+                                    //$id = $row["itemid"];
+                                    //$link = "edit_itemcategory.php?id=".$id;
+                                    echo "<tr><td>".$row["Room_code"]."</td>"."<td>".$row["Room_name"]."</td>"."<td>".$row["Division"]."</td>"."<td>".$row["Description"]."</td>";
+                                    echo "<td><a href='edit_room.php?id=".$row['Room_code']."'>Edit</a></td>";
+                                    //echo ("<td><button class='btn btn-success' href='edit_itemcategory.php?itemid=".$row['itemid']."'><i class='icon-ok'></i>Edit</button></td>");
+                                    echo "<td><a href='delete_room.php?id=".$row['Room_code']."'>Delete</a></td><tr>";
+
+                                    ////echo ("<td><button class='btn btn-danger' data-toggle='modal' data-target='#myModal2'><i class='icon-warning-sign'></i>
+                                          // Delete</a></button></td> </tr>");
+                                }
+                            }else{
+                                echo "0 results";
+                            }
+                            ?>
+                    </tbody>
+                    
+                </div>
+              </div>
+          </div>
+            </table>
+                  </form>
+              </div></div></div>
+        <div id="custom_notifications" class="custom-notifications dsp_none">
+          <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+          </ul>
+          <div class="clearfix"></div>
+          <div id="notif-group" class="tabbed_notifications"></div>
+        </div>
+          
+            <footer>
+              <div class="pull-right">
+                UCSC Asset Management System
+              </div>
+              <div class="clearfix"></div>
+            </footer>
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- bootstrap progress js -->
+        <script src="js/progressbar/bootstrap-progressbar.min.js"></script>
+        <!-- icheck -->
+        <script src="js/icheck/icheck.min.js"></script>
+
+        <script src="js/custom.js"></script>
+
+
+        <!-- Datatables -->
+        <!-- <script src="js/datatables/js/jquery.dataTables.js"></script>
+  <script src="js/datatables/tools/js/dataTables.tableTools.js"></script> -->
+
+        <!-- Datatables-->
+        <script src="js/datatables/jquery.dataTables.min.js"></script>
+        <script src="js/datatables/dataTables.bootstrap.js"></script>
+        <script src="js/datatables/dataTables.buttons.min.js"></script>
+        <script src="js/datatables/buttons.bootstrap.min.js"></script>
+        <script src="js/datatables/jszip.min.js"></script>
+        <script src="js/datatables/pdfmake.min.js"></script>
+        <script src="js/datatables/vfs_fonts.js"></script>
+        <script src="js/datatables/buttons.html5.min.js"></script>
+        <script src="js/datatables/buttons.print.min.js"></script>
+        <script src="js/datatables/dataTables.fixedHeader.min.js"></script>
+        <script src="js/datatables/dataTables.keyTable.min.js"></script>
+        <script src="js/datatables/dataTables.responsive.min.js"></script>
+        <script src="js/datatables/responsive.bootstrap.min.js"></script>
+        <script src="js/datatables/dataTables.scroller.min.js"></script>
+
+
+        <!-- pace -->
+        <script src="js/pace/pace.min.js"></script>
+        <script>
+          var handleDataTableButtons = function() {
+              "use strict";
+              0 !== $("#datatable-buttons").length && $("#datatable-buttons").DataTable({
+                dom: "Bfrtip",
+                buttons: [{
+                  extend: "copy",
+                  className: "btn-sm"
+                }, {
+                  extend: "csv",
+                  className: "btn-sm"
+                }, {
+                  extend: "excel",
+                  className: "btn-sm"
+                }, {
+                  extend: "pdf",
+                  className: "btn-sm"
+                }, {
+                  extend: "print",
+                  className: "btn-sm"
+                }],
+                responsive: !0
+              })
+            },
+            TableManageButtons = function() {
+              "use strict";
+              return {
+                init: function() {
+                  handleDataTableButtons()
+                }
+              }
+            }();
+        </script>
+        <script type="text/javascript">
+          $(document).ready(function() {
+            $('#datatable').dataTable();
+            $('#datatable-keytable').DataTable({
+              keys: true
+            });
+            $('#datatable-responsive').DataTable();
+            $('#datatable-scroller').DataTable({
+              ajax: "js/datatables/json/scroller-demo.json",
+              deferRender: true,
+              scrollY: 380,
+              scrollCollapse: true,
+              scroller: true
+            });
+            var table = $('#datatable-fixed-header').DataTable({
+              fixedHeader: true
+            });
+          });
+          TableManageButtons.init();
+        </script>
+</body>
+
+</html>
